1e715e1563ed8538b70cb33a6e0b15ba285389be
Timothy Bastow <timothy.bastow@outlook.com>
2023-10-29 11:12:07 +0100

    bug fix

--- agent.py
+++ agent.py
@@ -11,53 +11,6 @@ BATCH_SIZE = 1000
 LR = 0.001
 
 
-def get_state(game):
-    head = game.snake[0]
-    point_l = Point(head.x - 20, head.y)
-    point_r = Point(head.x + 20, head.y)
-    point_u = Point(head.x, head.y - 20)
-    point_d = Point(head.x, head.y + 20)
-
-    dir_l = game.direction == Direction.LEFT
-    dir_r = game.direction == Direction.RIGHT
-    dir_u = game.direction == Direction.UP
-    dir_d = game.direction == Direction.DOWN
-
-    state = [
-        # Danger straight
-        (dir_r and game.is_collision(point_r)) or
-        (dir_l and game.is_collision(point_l)) or
-        (dir_u and game.is_collision(point_u)) or
-        (dir_d and game.is_collision(point_d)),
-
-        # Danger right
-        (dir_u and game.is_collision(point_r)) or
-        (dir_d and game.is_collision(point_l)) or
-        (dir_l and game.is_collision(point_u)) or
-        (dir_r and game.is_collision(point_d)),
-
-        # Danger left
-        (dir_d and game.is_collision(point_r)) or
-        (dir_u and game.is_collision(point_l)) or
-        (dir_r and game.is_collision(point_u)) or
-        (dir_l and game.is_collision(point_d)),
-
-        # Move direction
-        dir_l,
-        dir_r,
-        dir_u,
-        dir_d,
-
-        # Food location
-        game.food.x < game.head.x,  # food left
-        game.food.x > game.head.x,  # food right
-        game.food.y < game.head.y,  # food up
-        game.food.y > game.head.y  # food down
-    ]
-
-    return np.array(state, dtype=int)
-
-
 class Agent:
 
     def __init__(self):
@@ -67,6 +20,52 @@ class Agent:
         self.memory = deque(maxlen=MAX_MEMORY)  # popleft()
         self.model = Linear_QNet(11, 256, 3)
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+
+    def get_state(self, game):
+        head = game.snake[0]
+        point_l = Point(head.x - 20, head.y)
+        point_r = Point(head.x + 20, head.y)
+        point_u = Point(head.x, head.y - 20)
+        point_d = Point(head.x, head.y + 20)
+
+        dir_l = game.direction == Direction.LEFT
+        dir_r = game.direction == Direction.RIGHT
+        dir_u = game.direction == Direction.UP
+        dir_d = game.direction == Direction.DOWN
+
+        state = [
+            # Danger straight
+            (dir_r and game.is_collision(point_r)) or
+            (dir_l and game.is_collision(point_l)) or
+            (dir_u and game.is_collision(point_u)) or
+            (dir_d and game.is_collision(point_d)),
+
+            # Danger right
+            (dir_u and game.is_collision(point_r)) or
+            (dir_d and game.is_collision(point_l)) or
+            (dir_l and game.is_collision(point_u)) or
+            (dir_r and game.is_collision(point_d)),
+
+            # Danger left
+            (dir_d and game.is_collision(point_r)) or
+            (dir_u and game.is_collision(point_l)) or
+            (dir_r and game.is_collision(point_u)) or
+            (dir_l and game.is_collision(point_d)),
+
+            # Move direction
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
+
+            # Food location
+            game.food.x < game.head.x,  # food left
+            game.food.x > game.head.x,  # food right
+            game.food.y < game.head.y,  # food up
+            game.food.y > game.head.y  # food down
+        ]
+
+        return np.array(state, dtype=int)
 
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))  # popleft if MAX_MEMORY is reached
@@ -110,14 +109,14 @@ def train():
     game = SnakeGameAI()
     while True:
         # get old state
-        state_old = get_state(game)
+        state_old = agent.get_state(game)
 
         # get move
         final_move = agent.get_action(state_old)
 
         # perform move and get new state
         reward, done, score = game.play_step(final_move)
-        state_new = get_state(game)
+        state_new = agent.get_state(game)
 
         # train short memory
         agent.train_short_memory(state_old, final_move, reward, state_new, done)
